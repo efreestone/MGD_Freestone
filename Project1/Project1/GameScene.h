8d45ec7aba8125f33641558e94a42ffa900d4acb
efreestone <efreestone@fullsail.edu>
2014-10-30 17:43:19 -0600

    After having some trouble understanding how to use Cocos2d w/ both SpriteBuilder and xCode, I decided to change to SpriteKit due to time constraints. The newest version of Cocos2d requires the use of SpriteBuilder to be used with the newest xCode.

---
+++ Project1/Project1/GameScene.h
@@ -0,0 +1,18 @@
+// Elijah Freestone
+// MGD 1411
+// Week 1
+// October 30th, 2014
+
+//
+//  GameScene.h
+//  Project1
+//
+
+//  Copyright (c) 2014 Elijah Freestone. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
---
+++ Project1/Project1/GameScene.m
@@ -0,0 +1,54 @@
+// Elijah Freestone
+// MGD 1411
+// Week 1
+// October 30th, 2014
+
+//
+//  GameScene.m
+//  Project1
+//
+//  Created by Elijah Freestone on 10/30/14.
+//  Copyright (c) 2014 Elijah Freestone. All rights reserved.
+//
+
+#import "GameScene.h"
+
+@implementation GameScene
+
+-(void)didMoveToView:(SKView *)view {
+    /* Setup your scene here */
+    SKLabelNode *myLabel = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
+    
+    myLabel.text = @"Hello, World!";
+    myLabel.fontSize = 65;
+    myLabel.position = CGPointMake(CGRectGetMidX(self.frame),
+                                   CGRectGetMidY(self.frame));
+    
+    [self addChild:myLabel];
+}
+
+-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+    /* Called when a touch begins */
+    
+    for (UITouch *touch in touches) {
+        CGPoint location = [touch locationInNode:self];
+        
+        SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithImageNamed:@"Spaceship"];
+        
+        sprite.xScale = 0.5;
+        sprite.yScale = 0.5;
+        sprite.position = location;
+        
+        SKAction *action = [SKAction rotateByAngle:M_PI duration:1];
+        
+        [sprite runAction:[SKAction repeatActionForever:action]];
+        
+        [self addChild:sprite];
+    }
+}
+
+-(void)update:(CFTimeInterval)currentTime {
+    /* Called before each frame is rendered */
+}
+
+@end
