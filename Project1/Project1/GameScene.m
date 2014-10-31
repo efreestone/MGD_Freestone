15d3f391ea70c22be29cc0244ccaf7bf7c4eb75a
efreestone <efreestone@fullsail.edu>
2014-10-31 13:59:36 -0600

    Set up vector math for projectile trojectory, replaced red laser w/ laser ball and asteroid with ufo

--- Project1/Project1/GameScene.m
+++ Project1/Project1/GameScene.m
@@ -16,8 +16,34 @@
 //Create private interface and variable for player fighter jet
 @interface GameScene ()
 @property (nonatomic) SKSpriteNode *playerFighterJet;
+@property (nonatomic) NSTimeInterval lastSpawnTimeInterval;
+@property (nonatomic) NSTimeInterval lastUpdateTimeInterval;
 
 @end
+
+//Add standard implementations of vector math routines for projectile trajectory
+static inline CGPoint rwAdd(CGPoint a, CGPoint b) {
+    return CGPointMake(a.x + b.x, a.y + b.y);
+}
+
+static inline CGPoint rwSub(CGPoint a, CGPoint b) {
+    return CGPointMake(a.x - b.x, a.y - b.y);
+}
+
+static inline CGPoint rwMult(CGPoint a, float b) {
+    return CGPointMake(a.x * b, a.y * b);
+}
+
+static inline float rwLength(CGPoint a) {
+    return sqrtf(a.x * a.x + a.y * a.y);
+}
+
+// Makes a vector have a length of 1
+static inline CGPoint rwNormalize(CGPoint a) {
+    float length = rwLength(a);
+    return CGPointMake(a.x / length, a.y / length);
+}
+
 
 @implementation GameScene
 
@@ -31,17 +57,16 @@
         
         //Add the fighter sprite to the scene w/ postion based on width of gfighter and height of frame
         self.playerFighterJet = [SKSpriteNode spriteNodeWithImageNamed:@"fighter"];
-        self.playerFighterJet.position = CGPointMake(self.playerFighterJet.size.width, self.frame.size.height/2);
+        self.playerFighterJet.position = CGPointMake(75, self.frame.size.height / 2);
         [self addChild:self.playerFighterJet];
     }
     return self;
 }
 
-//Add enemy objects to the scene and ani
+//Add enemy objects to the scene with random speed and spwan point
 -(void)addEnemy {
-    
     //Create enemy sprite
-    SKSpriteNode *enemy = [SKSpriteNode spriteNodeWithImageNamed:@"asteroid-1"];
+    SKSpriteNode *enemy = [SKSpriteNode spriteNodeWithImageNamed:@"spaceship"];
     
     //Create a random Y axis to spawn enemy
     int minimumY = enemy.size.height / 2;
@@ -53,53 +78,81 @@
     enemy.position = CGPointMake(self.frame.size.width + enemy.size.width/2, actualYAxis);
     [self addChild:enemy];
     
-    //Determine varied speed of enemies
+    //Determine varied speed of enemies from right to left
     int minDuration = 2.0;
     int maxDuration = 4.0;
     int rangeDuration = maxDuration - minDuration;
     int actualDuration = (arc4random() % rangeDuration) + minDuration;
     
-    //Create the actions
+    //Create move action from right to left and remove enemy once off screen
     SKAction *actionMove = [SKAction moveTo:CGPointMake(-enemy.size.width/2, actualYAxis) duration:actualDuration];
     SKAction * actionMoveDone = [SKAction removeFromParent];
     [enemy runAction:[SKAction sequence:@[actionMove, actionMoveDone]]];
     
 }
 
-//-(void)didMoveToView:(SKView *)view {
-//    /* Setup your scene here */
-//    SKLabelNode *myLabel = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
-//    
-//    myLabel.text = @"Hello, World!";
-//    myLabel.fontSize = 65;
-//    myLabel.position = CGPointMake(CGRectGetMidX(self.frame),
-//                                   CGRectGetMidY(self.frame));
-//    
-//    [self addChild:myLabel];
-//}
+//track time since last spawn and add new enemy every 1 second
+- (void)updateWithTimeSinceLastUpdate:(CFTimeInterval)timeSinceLast {
+    
+    self.lastSpawnTimeInterval += timeSinceLast;
+    if (self.lastSpawnTimeInterval > 1) {
+        self.lastSpawnTimeInterval = 0;
+        [self addEnemy];
+    }
+}
 
-//-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
-//    /* Called when a touch begins */
-//    
-//    for (UITouch *touch in touches) {
-//        CGPoint location = [touch locationInNode:self];
-//        
-//        SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithImageNamed:@"Spaceship"];
-//        
-//        sprite.xScale = 0.5;
-//        sprite.yScale = 0.5;
-//        sprite.position = location;
-//        
-//        SKAction *action = [SKAction rotateByAngle:M_PI duration:1];
-//        
-//        [sprite runAction:[SKAction repeatActionForever:action]];
-//        
-//        [self addChild:sprite];
-//    }
-//}
-//
-//-(void)update:(CFTimeInterval)currentTime {
-//    /* Called before each frame is rendered */
-//}
+//Called by SpriteKit every frame. Checks last update time which in turn spawns enemies.
+//This is from Apple's Adventure sample and includes logic to avoid odd behaviour if a large amount of time has passed.
+- (void)update:(NSTimeInterval)currentTime {
+    // Handle time delta.
+    // If we drop below 60fps, we still want everything to move the same distance.
+    CFTimeInterval timeSinceUpdate = currentTime - self.lastUpdateTimeInterval;
+    self.lastUpdateTimeInterval = currentTime;
+    if (timeSinceUpdate > 1) { // more than a second since last update
+        timeSinceUpdate = 1.0 / 60.0;
+        self.lastUpdateTimeInterval = currentTime;
+    }
+    
+    //Check time since last update and spawn accordingly
+    [self updateWithTimeSinceLastUpdate:timeSinceUpdate];
+    
+}
+
+//Grab touch event and calculate trajectory of projectile.
+//Also extends trajectory so the laser ball continues passed the touch and off screen.
+-(void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event {
+    //Grab touch and location within node
+    UITouch *touch = [touches anyObject];
+    CGPoint location = [touch locationInNode:self];
+    
+    //Set initial location of projectile to the fighter
+    SKSpriteNode *laserBall = [SKSpriteNode spriteNodeWithImageNamed:@"laser-ball"];
+    laserBall.position = self.playerFighterJet.position;
+    
+    //Determine offset of location to fighter
+    CGPoint offset = rwSub(location, laserBall.position);
+    
+    //Bail out if shooting down or backwards
+    if (offset.x <= 0) return;
+    
+    //Position has been double-checked, add laser ball sprite
+    [self addChild:laserBall];
+    
+    //Get the direction of where to shoot laser ball
+    CGPoint directionOfShot = rwNormalize(offset);
+    
+    //Shoot far enough for the laser ball to leave the screen
+    CGPoint shootOffScreen = rwMult(directionOfShot, 1000);
+    
+    //Add the shoot amount to the current position
+    CGPoint realDestination = rwAdd(shootOffScreen, laserBall.position);
+    
+    //Set velocity and create actions for the laser ball
+    float velocity = 480.0/1.0;
+    float realMoveDuration = self.size.width / velocity;
+    SKAction *actionMove = [SKAction moveTo:realDestination duration:realMoveDuration];
+    SKAction *actionMoveDone = [SKAction removeFromParent];
+    [laserBall runAction:[SKAction sequence:@[actionMove, actionMoveDone]]];
+}
 
 @end
